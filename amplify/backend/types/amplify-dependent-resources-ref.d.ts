04155395f20f520742473af1b680c85404289ea8
rusa3046 <aanya.rustogi@gmail.com>
2024-06-16 14:53:33 -0700

    fixed processstudymaterial and createfolder to work with html tester

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -14,6 +14,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "createFolder": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "generateStudySchedule": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
